fc56ec5f1de8c150df03e26306dd57951cbb3ac3
Yiteng Tian <yiteng.tian@uconn.edu>
2017-01-03 03:42:19 +0000

    specify api-key

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 # Config Details for https://ticker-window.herokuapp.com
 
 # API Keys
-QUANDL_API_KEY = 'xsBBBdJpJKG8oo8KJCoY'
-GOOGLE_MAPS_API_KEY = 'AIzaSyA4ueqgBsXoH5vf-zP04uxSxdGOf8AU1nA'
+QUANDL_API_KEY = 'u8TvzD9d5b_E3Ceft5xz'
