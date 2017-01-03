96946acaa7a5df03582ee09d9146fc8e2a05663e
Yiteng Tian <yiteng.tian@uconn.edu>
2017-01-03 03:39:53 +0000

    use quandl instead of requests

--- app.py
+++ app.py
@@ -1,83 +1,56 @@
-
-
-from __future__ import print_function
-from flask import Flask, render_template, request, redirect
-import requests
+# Import Dependencies
+import config as con
 import datetime
+import quandl
 import pandas as pd
+import os
+from bokeh.charts import output_file, TimeSeries
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
-'''
-dates = []
-now = datetime.datetime.now()
-last_month =  int(str(now).split('-')[1])-1
-close_prices_list = []
-stock = 'GOOG'
-api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=u8TvzD9d5b_E3Ceft5xz' % stock
-session = requests.Session()
-session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-raw_data = session.get(api_url)
-for i,ele in enumerate(raw_data.json()["dataset"]['data']):
-	if int(str(ele[0]).split('-')[1]) == last_month:
-		close_prices_list.append(ele[4]) # field for closing price
-		dates.append(str(ele[0]))
-	if int(str(ele[0]).split('-')[1])< last_month:
-		break
-dates = dates[::-1]
-close_prices_list = close_prices_list[::-1]
-pd_dates = pd.to_datetime(dates)
-df = pd.DataFrame(close_prices_list, index = pd_dates)
-'''
+from flask import Flask, render_template, request, redirect, flash, url_for
+
 app = Flask(__name__)
-app.vars = {}
 
+app.config.from_object(__name__)
+
+# Load default config 
+app.config.update( dict(
+	SECRET_KEY = 'development key',
+	))
+
+# Initialize Dates method
+quandl.ApiConfig.api_key = con.QUANDL_API_KEY
+current_month = datetime.date.today().month
+prior_month = current_month - 1
+year = datetime.date.today().year
+
+
+# ---------------------------------------------------------------------------
 @app.route('/')
-def landing():
-  if request.method == 'GET':
-    return render_template('landing.html')
-@app.route('/index',methods=['POST'])
+def main():
+  return redirect('/index')
+
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  app.vars['name'] = request.form['name_lulu']
-  dates = []
-  now = datetime.datetime.now()
-  last_month =  int(str(now).split('-')[1])-1
+	# Pull POST data for 'ticker' into a local variable
+	if request.method == 'POST':
+		ticker = request.form['ticker']
 
-  close_prices_list = []
-  stock = app.vars['name']
-  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=XPJ-bHXLTF3CvTThXtzL' % stock
-  session = requests.Session()
-  session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-  raw_data = session.get(api_url)
+		ticker_data = quandl.get('WIKI/' + ticker, collapse='daily')
 
-  for i,ele in enumerate(raw_data.json()["dataset"]['data']):
-    if int(str(ele[0]).split('-')[1]) == last_month:
-                close_prices_list.append(ele[4]) # field for closing price
-                dates.append(str(ele[0]))
-    if int(str(ele[0]).split('-')[1])< last_month:
-                break
-  dates = dates[::-1]
-  close_prices_list = close_prices_list[::-1]
-  pd_dates = pd.to_datetime(dates)
+		# Create title for Bokeh plot 
+		title = ('Daily Closing Stock Price for ' + ticker + ', ' + str(prior_month) + '/' + str(year) + ' - ' + str(current_month) + '/' + str(year))
 
-  df = pd.DataFrame(close_prices_list, index = pd_dates) 
+		# Create bokeh plotting variable
+		ts_plot = TimeSeries(ticker_data.Close[-30:], title = title, xlabel = 'Date', ylabel = 'Price ($ USD)')
 
-#create the graph
-  x = list(range(0,  10+ 1))
-  fig = figure(title="Stock value last month",x_axis_label='date',y_axis_label='Price in $')
-  fig.line(range(len(close_prices_list)),close_prices_list, color='Black', line_width=2)
-  
-  #config resources for BokehJS
-  js_resources = INLINE.render_js()
-  css_resources = INLINE.render_css()
-  
-  #embedd html components
-  script, div = components(fig,INLINE)
-  html= render_template('embed.html', plot_script=script, plot_div=div,js_resources=js_resources,css_resources=css_resources)
-  return encode_utf8(html)
-  return str(df)
-  return render_template('index.html')
+		script, div = components(ts_plot)
+
+		return render_template('plot.html', script=script, div=div)
+	else:
+		return render_template('index.html')
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port=33507)
+  app.run(host='0.0.0.0', debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+# Config Details for https://ticker-window.herokuapp.com
+
+# API Keys
+QUANDL_API_KEY = 'xsBBBdJpJKG8oo8KJCoY'
+GOOGLE_MAPS_API_KEY = 'AIzaSyA4ueqgBsXoH5vf-zP04uxSxdGOf8AU1nA'
